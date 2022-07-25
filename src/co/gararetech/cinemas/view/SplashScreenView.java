4b42f9f38f82980d3bd737f520402eb2bf624e15
erisukmawan_ <erisukmawan03@gmail.com>
2022-07-25 22:25:01 +0700

    Hilangin bar splashscreen

--- src/co/gararetech/cinemas/view/SplashScreenView.java
+++ src/co/gararetech/cinemas/view/SplashScreenView.java
@@ -48,6 +48,7 @@ public class SplashScreenView extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
